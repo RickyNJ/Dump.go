25f9df3d45a94d830906ef26105b96ad731272d5
RickyNJ <rickynijsten@gmail.com>
2024-08-14 16:13:28 +0200

    added file creation

--- main.go
+++ main.go
@@ -1,30 +1,50 @@
 package main
 
 import (
- "fmt"
- "reflect"
-) 
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+	"reflect"
+)
 
+func GetHeaders[T any](items []T) []string {
+	if items == nil {
+		return []string{}
+	}
 
+	headers := []string{}
 
+	val := reflect.ValueOf(items[0]).Elem()
+	for i := 0; i < val.NumField(); i++ {
+		headers = append(headers, val.Type().Field(i).Name)
+	}
 
-func GetHeaders [T any](items []T) []string {
-    if items == nil {
-        return []string{}
+	return headers
+}
+
+func CreateFile(fileName string) (*os.File, error){
+	fileName = fileName + ".csv"
+	f, err := os.Create(fileName)
+	return f, err
+}
+
+func DumpToCSV[T any](fileName string, items []T) bool {
+
+	f, err  := CreateFile(fileName)
+    if err != nil {
+        log.Fatalf("Error when creating the file %s", err)
     }
 
-    headers := []string{}
+	headers := GetHeaders(items)
 
-    val := reflect.ValueOf(items[0]).Elem()
-    for i:=0; i<val.NumField();i++{
-        headers = append(headers,val.Type().Field(i).Name )
-    }
+	w := csv.NewWriter(f)
+	w.Write(headers)
+	w.Flush()
 
-    return headers
+	return true
 }
 
 func main() {
-    fmt.Print("hi")
+	fmt.Print("hi")
 }
-
-
--- main_test.go
+++ main_test.go
@@ -1,39 +1,54 @@
 package main
 
 import (
-    "testing"
-    "reflect"
+	"os"
+	"reflect"
+	"testing"
 )
 
 type Person struct {
-    Name string
-    Age int
+	Name string // hello
+	Age  int    // will go fmt format this for me
 }
 
 type Classes struct {
-    Name string
-    Count int 
-    People []*Person
-    Leader *Person
+	Name   string
+	Count  int
+	People []*Person
+	Leader *Person
 }
 
 func TestPrintNamesPerson(t *testing.T) {
-    test := []*Person{{Name: "Ricky", Age: 23}, {Name: "Alice", Age: 26}}
-    want := []string{"Name", "Age"}
+	test := []*Person{{Name: "Ricky", Age: 23}, {Name: "Alice", Age: 26}}
+	want := []string{"Name", "Age"}
 
-    ans := GetHeaders(test)
-    if !reflect.DeepEqual(ans, want) {
-        t.Fatalf("oh oh")
-    }
+	ans := GetHeaders(test)
+	if !reflect.DeepEqual(ans, want) {
+		t.Fatalf("oh oh")
+	}
 }
 
 func TestPrintHeadersClasses(t *testing.T) {
-    test := []*Classes{{Name: "A3", Count: 3, People: []*Person{{Name: "Ricky", Age: 23}, {Name: "Alice", Age: 26}}, Leader: &Person{Name: "Pekin", Age: 4}}}
-    want := []string{"Name", "Count", "People", "Leader"}
+	test := []*Classes{{Name: "A3", Count: 3, People: []*Person{{Name: "Ricky", Age: 23}, {Name: "Alice", Age: 26}}, Leader: &Person{Name: "Pekin", Age: 4}}}
+	want := []string{"Name", "Count", "People", "Leader"}
 
-    ans := GetHeaders(test)
+	ans := GetHeaders(test)
 
-    if !reflect.DeepEqual(ans, want) {
-        t.Fatalf(" oh no")
+	if !reflect.DeepEqual(ans, want) {
+		t.Fatalf(" oh no")
+	}
+}
+
+func TestFileCreation(t *testing.T) {
+    CreateFile("test")
+     
+    if _, err := os.Stat("test.csv"); os.IsNotExist(err) {
+        t.Fatalf("file doesnt exist: %s", err)	
     }
+
+    err := os.Remove("test.csv") 
+    if err != nil { 
+        t.Fatalf("Failed to remove the file: %v", err)
+    }
+
 }
