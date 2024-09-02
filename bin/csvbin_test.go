b28219f23a3f77be709f5240f197dd64bdf33416
RickyNJ <rickynijsten@gmail.com>
2024-09-02 20:52:22 +0200

    cleaned up csvbin tests

--- bin/csvbin_test.go
+++ bin/csvbin_test.go
@@ -8,15 +8,20 @@ import (
     "reflect"
 )
 
-func isEqual(input *os.File, want [][]string) bool {
-    r := csv.NewReader(input)
+func isEqual(input string, want [][]string) bool {
+    f, err := os.Open(input)
+    defer f.Close()
+    if err != nil {
+        fmt.Println(err)
+    }
+
+    r := csv.NewReader(f)
     record, err := r.ReadAll()
     if err != nil {
         fmt.Println(err)
     }
 
     fmt.Printf("testing for deepequal\ninput: %v \nwant: %v \n", record, want)
-
     return reflect.DeepEqual(record, want)
 }
 
@@ -24,60 +29,26 @@ func TestCSVTossWithNil(t *testing.T) {
     b := NewBin("people.csv", Person{})
     b.Toss(Person{Age: 12})
 
-    f, err := os.Open("people.csv")
-    if err != nil {
-        fmt.Println(err)
-    }
-
-    if !isEqual(f, [][]string{{"Name", "Age"},{"", "12"}}) {
+    if !isEqual("people.csv", [][]string{{"Name", "Age"},{"", "12"}}) {
         panic("not equal")
     }
 
 }
 func TestNewCSVBinCreation(t *testing.T) {
 	filename := "people.csv"
-	defer os.Remove(filename)
-
 	NewBin(filename, Person{})
-
-	f, err := os.Open(filename)
-	if err != nil {
-		fmt.Printf("%v is not created: %v", filename, err)
-	}
-
-	r := csv.NewReader(f)
-	record, err := r.Read()
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	if !reflect.DeepEqual(record, []string{"Name", "Age"}) {
-		t.Fatalf("csv content not correct")
-	}
-
+    if !isEqual("people.csv", [][]string{{"Name, Age"}}) {
+        panic("not equal")
+    }
 }
 
 
 func TestNewCSVBinCreationNestedStruct(t *testing.T) {
-	defer os.Remove("job.csv")
 	filename := "job.csv"
-
 	NewBin(filename, JobNested{})
-
-	f, err := os.Open(filename)
-	if err != nil {
-		fmt.Printf("%v is not created: %v", filename, err)
-	}
-
-	r := csv.NewReader(f)
-	record, err := r.Read()
-	if err != nil {
-		fmt.Println(err)
-	}
-
-    if !reflect.DeepEqual(record, []string{"Job:Person:Name", "Job:Person:Age", "Job:Company", "Job:Salary", "Name"}) {
-		t.Fatalf("csv content not correct")
-	}
+    if !isEqual("job.csv",  [][]string{{"Job:Person:Name", "Job:Person:Age", "Job:Company", "Job:Salary", "Name"}}) {
+        panic("not equal")
+    }
 }
 
 
@@ -85,28 +56,14 @@ func TestCSVToss(t *testing.T) {
 	b := NewBin("people.csv", PersonStrings{})
 	b.Toss(Person{Name: "hi", Age: 21})
 
-	file, err := os.Open("peoples.csv")
-	if err != nil {
-		t.Errorf("file doesnt exist %v", err)
-	}
-
-	r := csv.NewReader(file)
-
-	lines, err := r.ReadAll()
-	if err != nil {
-		t.Errorf("Couldnt read file %v", err)
-	}
 	want := [][]string{{"Name", "Age"}, {"hi", "21"}}
-	if !reflect.DeepEqual(lines, want) {
-		t.Fatalf("%v and %v are not equal", lines, want)
-	}
+    if !isEqual("people.csv", want) {
+        panic("not equal")
+    }
 }
 
 
-
-
 func TestCSVTossWithSlice(t *testing.T) {
-    defer os.Remove("peoples.csv")
 	b := NewBin("peoples.csv", Person{})
 
 	personslice := []Person{{Name: "Ricky", Age: 12}, {Name: "Alice", Age: 23}}
@@ -114,6 +71,12 @@ func TestCSVTossWithSlice(t *testing.T) {
 
 	b.Toss(personslice)
 	b.Toss(personarray)
+
+    want := [][]string{{"Name", "Age"}, {"Ricky", "12"}, {"Alice", "23"}, {"lkj", "12"}, {"lkdsjAlice", "23"}}
+
+    if !isEqual("peoples.csv", want) {
+        panic("not equal")
+    }
 }
 
 type arrstruct struct {
@@ -121,10 +84,13 @@ type arrstruct struct {
 }
 
 func TestCSVTossArr(t *testing.T) {
-    defer os.Remove("arr.csv")
-
 	b := NewBin("arr.csv", arrstruct{})
 	b.Toss(arrstruct{i: []string{"hi", "hello"}})
+    
+    want := [][]string{{"i"}, {"[hi hello]"}}
+    if !isEqual("arr.csv", want) {
+        panic("not equal")
+    }
 }
 
 type Nested struct {
